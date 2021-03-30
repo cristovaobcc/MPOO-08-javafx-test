373e722142b17911592096f6be36980b9502ac2d
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-30 16:41:25 -0300

    Cria a classe Alerts para exibir alertas nas views.

---
+++ src/gui/util/Alerts.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package gui.util;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+/**
+ * Classe com métodos para exibir Alerts do JavaFX.
+ *
+ */
+public class Alerts {
+	
+	/**
+	 * Exibe um Alert do tipo inserido e com os dados inseridos.
+	 * @param title String
+	 * @param header String
+	 * @param content String
+	 * @param type {@link AlertType}
+	 */
+	public static void showAlert(String title, String header, String content, AlertType type) {
+		Alert alert = new Alert(type);
+		alert.setTitle(title);
+		alert.setHeaderText(header);
+		alert.setContentText(content);
+		alert.show();
+	}
+
+}
