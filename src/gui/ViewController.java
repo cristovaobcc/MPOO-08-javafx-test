1765de5262a26088000d270a25da5c9ba7552c4d
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-30 17:25:19 -0300

    Exemplo de tratamento de erro em ViewController.

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -5,7 +5,9 @@ package gui;
 
 import java.util.Locale;
 
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -28,13 +30,19 @@ public class ViewController {
 	
 	@FXML // método que será acionado quando o botão btTest for clicado.
 	public void onBtSumAction() {
-		Locale.setDefault(Locale.US); // para garantir que o número inserido seja lido com o ponto como separador de decimais.
-		double number1 = Double.parseDouble(txtNumber1.getText());
-		double number2 = Double.parseDouble(txtNumber2.getText());
-		double sum = number1 + number2;
 		
-		labelResult.setText(String.format("%.2f", sum));
-		
+		try {
+			Locale.setDefault(Locale.US); // para garantir que o número inserido seja lido com o ponto como separador de decimais.
+			double number1 = Double.parseDouble(txtNumber1.getText());
+			double number2 = Double.parseDouble(txtNumber2.getText());
+			double sum = number1 + number2;
+
+			labelResult.setText(String.format("%.2f", sum));
+			
+		} catch (NumberFormatException e) {
+			Alerts.showAlert("Erro", "Erro de valor inserido", e.getMessage(), AlertType.ERROR);
+		}
+
 	}
 
 	
