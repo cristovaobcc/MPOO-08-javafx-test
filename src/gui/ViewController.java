9794a3774c24b19c58ae45ba7238f0bd617786fa
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-30 17:18:34 -0300

    Insere atributos correspondentes à nova view em ViewController.
    Também houve refatoração do método para corresponder à nova
    funcionalidade.

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -3,10 +3,12 @@
  */
 package gui;
 
-import gui.util.Alerts;
+import java.util.Locale;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  * @author cristovao
@@ -14,13 +16,25 @@ import javafx.scene.control.Button;
  */
 public class ViewController {
 	
+	@FXML
+	private TextField txtNumber1;
+	@FXML
+	private TextField txtNumber2;
+	@FXML
+	private Label labelResult;
+	
 	@FXML 
-	private Button btTest;  // Atributo correspondente ao elemento gráfico da tela.
+	private Button btSum;  // Atributo correspondente ao elemento gráfico da tela.
 	
 	@FXML // método que será acionado quando o botão btTest for clicado.
-	public void onBtTestAction() {
-		Alerts.showAlert("Alert title", "Alert header", "Hello Alert!", AlertType.INFORMATION);
-		Alerts.showAlert("Alert with no header", null, "Alert with no header", AlertType.WARNING);
+	public void onBtSumAction() {
+		Locale.setDefault(Locale.US); // para garantir que o número inserido seja lido com o ponto como separador de decimais.
+		double number1 = Double.parseDouble(txtNumber1.getText());
+		double number2 = Double.parseDouble(txtNumber2.getText());
+		double sum = number1 + number2;
+		
+		labelResult.setText(String.format("%.2f", sum));
+		
 	}
 
 	
